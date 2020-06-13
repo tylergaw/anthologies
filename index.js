3d1d9216a3dec68966e03eca06b9ce1c3117b932
Tyler Gaw <me@tylergaw.com>
2020-06-13 13:08:57 -0400

    Release landfill (#1)
    * Begins styles for Anthology
    * Adds base styles to entries
    * Fluid type and max line lengths
    * Adds branding element
    * Introducing images

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const fs = require("fs");
 const handlebars = require("handlebars");
 const BASE_TMPL = "./templates/base.html";
+// TODO: env config
+const ASSET_URL = "https://s3.amazonaws.com/anthologies-assets.dev/";
 
 const isLocal = process.env.AWS_SAM_LOCAL;
 const statusKey = isLocal ? "statusCode" : "status";
@@ -51,14 +53,30 @@ const Publications = {
   }
 };
 
-const Entries = {
+// TODO: Join. Instead of storing each article's details in the Entry, store
+// it in a separate table. That way we can reuse already parsed / corrected
+// articles.
+const Articles = {
   "1": {
-    id: "1",
     title: "The Silent Season of a Hero",
     authors: [1, 3],
     publication: 1,
     url: "http://www.randomhouse.com/kvpa/talese/essays/dimaggio.html",
-    description: "The complicated post-baseball days of Joe DiMaggio."
+    description: "The complicated post-baseball days of Joe DiMaggio.",
+    image: "a1-1.jpg"
+  }
+}
+
+const Entries = {
+  "1": {
+    id: "1",
+    title: "The Silent Season of a Hero",
+    article: 1,
+    authors: [1, 3],
+    publication: 1,
+    url: "http://www.randomhouse.com/kvpa/talese/essays/dimaggio.html",
+    description: "The complicated post-baseball days of Joe DiMaggio.",
+    image: "a1-1.jpg"
   },
   "2": {
     id: "2",
@@ -66,7 +84,8 @@ const Entries = {
     authors: [2],
     publication: 2,
     url: "https://www.washingtonpost.com/sports/wizards/allen-iverson-nba-icon-struggles-with-life-after-basketball/2013/04/19/bfd108f8-a76e-11e2-a8e2-5b98cb59187f_story.html",
-    description: "Basketball’s iconoclast is now a broke recluse at 37."
+    description: "The former NBA superstar is coping with life after basketball — and finding the adjustment difficult.",
+    image: "a2-1.jpg"
   },
   "3": {
     id: "3",
@@ -74,15 +93,17 @@ const Entries = {
     authors: [3],
     publication: 3,
     url: "https://www.si.com/vault/2009/03/23/105789480/how-and-why-athletes-go-broke",
-    description: "Five years after they leave the league, 60 percent of NBA players have nothing left. In the NFL, it’s closer to 80 percent after just two years. A breakdown of the economics of retirement."
+    description: "Recession or no recession, many NFL, NBA and Major League Baseball players have a penchant for losing most or all of their money. It doesn't matter how much they make. And the ways they blow it are strikingly similar",
+    image: null
   },
   "4": {
     id: "4",
-    title: "Love Me, Hate Me, Just Don’t Ignore Me",
+    title: "Terrell Owens, NFL Wide Receiver - GQ February 2012",
     authors: [4],
     publication: 4,
     url: "https://www.gq.com/story/terrell-owens-nfl-football-wide-receiver",
-    description: "Terrell Owens at 38: unemployed, nearly bankrupt after losing his shirt in a electronic-bingo entertainment complex development plan gone bust, father of four children (one of which he has never met), frequent bowler."
+    description: "As you're planning your Super Bowl party this year, give a thought to future Hall of Famer Terrell Owens. He's out of work, out of money, and currently in court with all four of his baby mamas. And now for the part that really depresses him: For the first time in his long, checkered, and spectacular career, nobody wants to throw him the ball.",
+    image: null
   }
 };
 
@@ -94,7 +115,8 @@ const anthologies = {
     description:
       "Iverson, Canseco, TO, and DiMaggio — a collection of picks on post-career woe. I think the average description will be longer than what was here before. Maybe about long tweet size at 280 characters?",
     entries: ["1", "2", "3", "4"],
-    curators: ["1"]
+    curators: ["1"],
+    image: "a1-1.jpg"
   },
   "456": { id: "456", title: "Anthology Title 2" },
   "789": { id: "789", title: "Anthology Title 3" },
@@ -102,43 +124,65 @@ const anthologies = {
 };
 
 const anthologyTmpl = `
-  <h1>{{title}}</h1>
-  <h3>Curated by <a href="/u/{{user.id}}">{{user.name}}</a></h3>
-  <p>{{description}}</p>
-  <ul>
-  {{#each entries}}
-    <li>
-      {{#each authors}}{{#unless @first}}, {{/unless}}{{name}}{{/each}}
-      • {{#publication}}<strong>{{name}}</strong>{{/publication}}
+  <div class="ant ant--spartan">
+    <header class="ant__cover {{#if image}}ant__cover--w-image{{/if}}">
+      {{#if image}}
+      <img class="cover__img" src="{{assetUrl}}{{image}}" alt="">
+      {{/if}}
+      <div class="cover__body">
+        <h1 class="ant__title">{{title}}</h1>
+        <h3 class="ant__byline">
+          <span class="text--subtle">Curated by</span> <a href="/u/{{user.id}}">{{user.name}}</a>
+        </h3>
+        <p class="ant__description">{{description}}</p>
+      </div>
+    </header>
+    <ul class="ant__entries">
+    {{#each entries}}
+      <li class="entry {{#if image}}entry--w-image{{/if}}">
+        <span class="text--subtle">{{#each authors}}{{#unless @first}}, {{/unless}}{{name}}{{/each}}</span>
+        • {{#publication}}<strong>{{name}}</strong>{{/publication}}
 
-      <a href="{{url}}" target="_blank" rel="noopener">
-        <h2>{{title}}</h2>
-        {{#description}}
-        <p>
-          {{this}}
-        </p>
-        {{/description}}
-      </a>
-    </li>
-  {{else}}
-    <li>No entries</li>
-  {{/each}}
-  </ul>
+        <a class="entry__link" href="{{url}}" target="_blank" rel="noopener">
+          <h2 class="entry__title">{{title}}</h2>
+          {{#description}}
+          <p class="entry__desc">
+            {{this}}
+          </p>
+          {{/description}}
+        </a>
+
+        {{#if image}}
+        <img src="{{assetUrl}}{{image}}" alt="">
+        {{/if}}
+      </li>
+    {{else}}
+      <li>No entries</li>
+    {{/each}}
+    </ul>
+    <div class="branding">
+      <a href="/">anthologies</a>
+    </div>
+  </div>
 `;
 
 const Anthology = a => {
-  const { curators, entries, title, description } = a;
+  const { curators, entries, title, description, image } = a;
 
   return {
     title,
     contents: handlebars.compile(anthologyTmpl)({
       title,
       description,
+      image,
+      assetUrl: ASSET_URL,
       entries: entries.map(entryId => {
-        const { id, description, url, title, authors, publication } = Entries[entryId];
+        const { id, image, description, url, title, authors, publication } = Entries[entryId];
 
         return {
+          assetUrl: ASSET_URL,
           id,
+          image,
           title,
           url,
           description,
